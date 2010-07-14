b1d691aab24e31b26d695de83b39f6e0f7712f37
Macpiece <macpiece@Macpieces-MacBook.local>
2010-07-14 15:00:01 -0700

    removed old formatting

--- routes.rb
+++ routes.rb
@@ -1,4 +1,4 @@
-resources :faqs
+resources :faqs, :member => { :tag => :get }
 namespace :admin do |admin|
-  admin.resources :faqs, :member => { :reorder => :put }
+  admin.resources :faqs, :member => { :reorder => :put }, :collection => { :update_positions => :put }
 end 
